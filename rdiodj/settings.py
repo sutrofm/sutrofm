d852ea77e411da066e9c6de83fd6dff43440095f
Devin Sevilla <devin.sevilla@rd.io>
2013-02-26 16:30:51 -0800

    Add rdioparty.herokuapp.com to ALLOWED_HOSTS

--- rdiodj/settings.py
+++ rdiodj/settings.py
@@ -20,7 +20,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.4/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rdioparty.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
