5ac26120d27e37dd0ea1ceb10c3e4d877e8266a0
Devin Sevilla <devin.sevilla@rd.io>
2013-03-03 13:21:40 -0800

    Update static files config

--- rdiodj/settings.py
+++ rdiodj/settings.py
@@ -66,14 +66,14 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'rdiodj'
 
 
-
 if DEBUG:
     STATIC_URL = '/static/'
 else:
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
