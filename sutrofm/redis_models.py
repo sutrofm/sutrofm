09d02a0d3ef0fb3c52ceadee92eec3bc2883934f
Marek Kapolka <marek.kapolka@gmail.com>
2015-06-04 17:31:44 -0700

    Party user lists

--- sutrofm/redis_models.py
+++ sutrofm/redis_models.py
@@ -3,26 +3,36 @@
 class Party(object):
   def __init__(self):
     self.id = None
-    self.name = "No Name"
+    self.name = "unnamed"
     self.playingTrackId = None
     self.playingTrackPosition = 0
+    self.users = []
 
   @staticmethod
   def get(connection, id):
     data = connection.hgetall('parties:%s' % id)
-    output = Party()
-    output.id = id
-    output.name = data.get('name', 'No name')
-    output.playingTrackId = data.get('playingTrackId', None)
-    output.playingTrackPosition = data.get('playingTrackId', 0)
-    return output
+    if data:
+        output = Party()
+        output.id = id
+        output.name = data.get('name', 'No name')
+        output.playingTrackId = data.get('playingTrackId', None)
+        output.playingTrackPosition = data.get('playingTrackId', 0)
 
-    @staticmethod
-    def getall(connection):
-      ids = connection.smembers('parties')
-      return [
-        Party.get(connection, i) for i in ids
-      ]
+        # Get users
+        user_keys = connection.smembers('party:users:%s' % id)
+        output.users = [
+            User.get(key) for key in user_keys
+        ]
+        return output
+    else:
+        return None
+
+  @staticmethod
+  def getall(connection):
+    ids = connection.smembers('parties')
+    return [
+      Party.get(connection, i) for i in ids
+    ]
 
   def save(self, connection):
     if not hasattr(self, 'id'):
@@ -32,7 +42,18 @@ class Party(object):
       "playingTrackId": self.playingTrackId,
       "playingTrackPosition": self.playingTrackPosition
     })
+    # Save users
+    for user in self.users:
+        connection.sadd('party:users:%s' % self.id, user.id)
     connection.sadd('parties', self.id)
+
+  def add_user(self, user):
+    if user not in self.users:
+        self.users.append(user)
+
+  def remove_user(self, user):
+    if user in self.users:
+      self.users.remove(user)
 
 class User(object):
     @staticmethod
