1c5a3f3a169243e495028ff75aeb939ad7bfe78f
Ubuntu <ubuntu@ip-10-231-6-19.us-west-2.compute.internal>
2015-09-22 17:52:27 +0000

    WSGI script files

--- sutrofm/wsgi.py
+++ sutrofm/wsgi.py
@@ -25,10 +25,10 @@ from django.conf import settings
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
 
 _django_app = get_wsgi_application()
-_websocket_app = uWSGIWebsocketServer()
+# _websocket_app = uWSGIWebsocketServer()
 
 
 def application(environ, start_response):
-  if environ.get('PATH_INFO').startswith(settings.WEBSOCKET_URL):
-    return _websocket_app(environ, start_response)
+  # if environ.get('PATH_INFO').startswith(settings.WEBSOCKET_URL):
+    # return _websocket_app(environ, start_response)
   return _django_app(environ, start_response)
