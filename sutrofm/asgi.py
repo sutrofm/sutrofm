1b2f8dd87c855d3b330b55c64109949417f659c5
aaroncohen <cohena+aaroncohen@gmail.com>
2020-04-19 20:58:23 -0700

    Fix heroku websocket?

--- sutrofm/asgi.py
+++ sutrofm/asgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.default')
 
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
