8ab59382c225b1592c7918a37919433580def305
aaroncohen <cohena+aaroncohen@gmail.com>
2020-04-20 10:49:00 -0700

    Set django to not keep connections open to postgres.

--- settings/staging.py
+++ settings/staging.py
@@ -19,7 +19,7 @@ CHANNEL_LAYERS = {
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 
-DATABASES = {'default': dj_database_url.config(conn_max_age=10)}
+DATABASES = {'default': dj_database_url.config(conn_max_age=0)}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
